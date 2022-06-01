fbbbc2c3fe5b00ede78776dd7ff7fac8206bb709
dikxak <dikshakpoudel76@gmail.com>
2022-06-01 17:33:15 +0545

    add landing page file

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from '../components/ui/Navbar';
+
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
+
+export default LandingPage;
