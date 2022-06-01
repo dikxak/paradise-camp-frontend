e0a260aeb85b388a36db1d50a8f640b711321ab7
dikxak <dikshakpoudel76@gmail.com>
2022-06-01 21:50:16 +0545

    add hero section component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Navbar from '../components/ui/Navbar';
+import HeroSection from '../components/landing_page/HeroSection';
 
 const LandingPage = () => {
   return (
     <React.Fragment>
       <Navbar />
+      <HeroSection />
     </React.Fragment>
   );
 };
