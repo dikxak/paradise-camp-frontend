378340791d9b9728f8c1e58cbc25e088904337e5
dikxak <dikshakpoudel76@gmail.com>
2022-06-01 17:32:46 +0545

    add navbar component

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import logo256 from '../../assets/images/logo-256.png';
+
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={`navbar navbar-expand-lg ${styles['bg-custom']}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand py-1 mx-4" href="/">
+          <img
+            src={logo256}
+            alt="ParadiseCamp Logo"
+            width="64"
+            height="64"
+            className="d-inline-block align-text-center"
+          />
+          <span className={styles['logo-text']}>ParadiseCamp</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse`} id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto py-2 mx-5 text-center">
+            <li className="nav-item mx-3">
+              <a
+                className="nav-link nav-link-login px-3 py-3"
+                aria-current="page"
+                href="/"
+              >
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={`nav-link ${styles['custom-nav-link']} ${styles['nav-link-signup']} px-3 py-3`}
+                href="/"
+              >
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
