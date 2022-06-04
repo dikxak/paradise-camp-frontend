183a7d8672aaafd9f6f06d42f59c240dd2261610
dikxak <dikshakpoudel76@gmail.com>
2022-06-04 18:46:37 +0545

    wrap App component with react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { Routes, Route } from 'react-router-dom';
+
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 };
 
