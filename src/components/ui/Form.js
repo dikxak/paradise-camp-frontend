10515fa0f964584931ff2fc90dcaa39d20fc6d65
dikxak <dikshakpoudel76@gmail.com>
2022-06-04 18:50:42 +0545

    add Form component

---
+++ src/components/ui/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Card from './Card';
+
+import styles from './Form.module.css';
+
+const Form = props => {
+  return (
+    <Card className={styles['form-container']}>
+      <img className={styles['form-img']} src={props.img} alt="Campfire" />
+      <h2>{props.heading}</h2>
+      <form className={styles['form']}>{props.children}</form>
+    </Card>
+  );
+};
+
+export default Form;
