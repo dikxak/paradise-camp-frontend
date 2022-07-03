9b82c2fd0f085adeacf1041b6af350a495527128
dikxak <dikshakpoudel76@gmail.com>
2022-07-03 17:57:09 +0545

    import Navbar to HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/ui/Navbar/Navbar';
 
 const HomePage = props => {
-  return <h2>Home Page</h2>;
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
