adff2878c5b9af7ec2769f5283b77282a3d98a37
dikxak <dikshakpoudel76@gmail.com>
2022-07-12 14:05:34 +0545

    import and use HomeSection component in HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Navbar from '../../components/ui/Navbar/Navbar';
+import HomeSection from './HomeSection/HomeSection';
 
 const HomePage = props => {
+  const [spotDataPicnic, setSpotDataPicnic] = useState([]);
+  const [spotDataCamping, setSpotDataCamping] = useState([]);
+
+  const getSpotData = async type => {
+    const res = await axios.get(`http://localhost:90/spots/type=${type}`);
+    console.log(res.data);
+    return res.data.data;
+  };
+
+  useEffect(() => {
+    getSpotData('Picnic')
+      .then(data => {
+        setSpotDataPicnic(data);
+      })
+      .catch(err => console.log(err));
+
+    getSpotData('Camping')
+      .then(data => {
+        setSpotDataCamping(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <React.Fragment>
       <Navbar />
+      <HomeSection sectionHeading={'Picnic Spots'} spotData={spotDataPicnic} />
+      <HomeSection
+        sectionHeading={'Camping Spots'}
+        spotData={spotDataCamping}
+      />
     </React.Fragment>
   );
 };
