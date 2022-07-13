57522dba01797c57b0d2c1fe24fc9310ee28f8be
dikxak <dikshakpoudel76@gmail.com>
2022-07-13 13:12:38 +0545

    update class names in component and css files

--- src/pages/HomePage/HomeSection/HomeSection.js
+++ src/pages/HomePage/HomeSection/HomeSection.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Card from '../../../components/ui/Card/Card';
 
 import styles from './HomeSection.module.css';
+import homeStyles from '../HomePage.module.css';
 
 const HomeSection = props => {
-  console.log(props.spotData);
   return (
     <section className="container overflow-hidden">
-      <h3 className={styles['spot-heading']}>{props.sectionHeading}</h3>
+      <h3 className={homeStyles['home-heading']}>{props.sectionHeading}</h3>
       <div className="row row-cols-1 row-cols-md-1 row-cols-lg-2 gx-2 gy-5">
         {props.spotData.map(data => {
           return (
@@ -17,14 +17,14 @@ const HomeSection = props => {
               key={data._id}
             >
               <Card
-                className={`${styles['picnic-spot-container']} grid grid--2-cols`}
+                className={`${homeStyles['home-container']} grid grid--2-cols`}
               >
                 <img
                   src={data.imageURL}
                   alt="Night Sky"
-                  className={styles['picnic-spot-img']}
+                  className={`${homeStyles['home-img']} ${styles['spot-img']}`}
                 />
-                <div className="picnic-spot-contents">
+                <div className={styles['spot-contents']}>
                   <h4 className={styles['spot-name']}>{data.name}</h4>
                   <p className={`${styles['spot-description']}`}>
                     {data.description}
