dca4f8bbb7b518d46420bd144e5f3eec20bb8e52
dikxak <dikshakpoudel76@gmail.com>
2022-07-19 11:10:20 +0545

    make component accept the className in prop

--- src/components/ui/Form/Form.js
+++ src/components/ui/Form/Form.js
@@ -6,7 +6,7 @@ import styles from './Form.module.css';
 
 const Form = props => {
   return (
-    <Card className={styles['form-container']}>
+    <Card className={`${styles['form-container']} ${props.className}`}>
       <img className={styles['form-img']} src={props.img} alt="Campfire" />
       <h2>{props.heading}</h2>
       <form onSubmit={props.onSubmit} className={styles['form']}>
