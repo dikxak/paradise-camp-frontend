09c4b0cedbc0b817978c392108fa0ee6aef8da47
dikxak <dikshakpoudel76@gmail.com>
2022-07-19 11:08:46 +0545

    add Select component

---
+++ src/components/ui/Select/Select.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Select.module.css';
+
+const Select = props => {
+  return (
+    <div className={styles.control}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <select onChange={props.onChanged} name="spotTypes" id={props.id}>
+        <option value="picnic">Picnic</option>
+        <option value="camping">Camping</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
