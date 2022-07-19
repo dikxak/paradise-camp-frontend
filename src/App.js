41d084f6a3b79ebbbd91052fb07c3ba6b68d98c7
dikxak <dikshakpoudel76@gmail.com>
2022-07-19 11:12:52 +0545

    add path for adding location and modify path according to user's login status

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import HomePage from './pages/HomePage/HomePage';
+import AddLocationPage from './pages/AddLocationPage/AddLocationPage';
 
 const App = () => {
   return (
     <LoginContextProvider>
       <LoadingContextProvider>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/"
+            element={
+              localStorage.getItem('token') ? <HomePage /> : <LandingPage />
+            }
+          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/location/add" element={<AddLocationPage />} />
         </Routes>
       </LoadingContextProvider>
     </LoginContextProvider>
