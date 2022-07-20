879227427a2fa596e2322b9d5a554ffe7f2485f8
dikxak <dikshakpoudel76@gmail.com>
2022-07-20 11:46:27 +0545

    add icons required for the project

---
+++ src/components/icons/MobileIcon.js
@@ -0,0 +1,19 @@
+import './Icon.css';
+
+const MobileIcon = props => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="192"
+      height="192"
+      fill="#000000"
+      viewBox="0 0 256 256"
+      className={`icon ${props.className}`}
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path d="M176,16H80A24.1,24.1,0,0,0,56,40V216a24.1,24.1,0,0,0,24,24h96a24.1,24.1,0,0,0,24-24V40A24.1,24.1,0,0,0,176,16ZM80,32h96a8,8,0,0,1,8,8v8H72V40A8,8,0,0,1,80,32Zm96,192H80a8,8,0,0,1-8-8v-8H184v8A8,8,0,0,1,176,224Z"></path>
+    </svg>
+  );
+};
+
+export default MobileIcon;
