2f02730aa1e18166d7364ab8ff62183b36e54d7f
dikxak <dikshakpoudel76@gmail.com>
2022-07-21 18:26:30 +0545

    add value prop to the Select component

--- src/components/ui/Select/Select.js
+++ src/components/ui/Select/Select.js
@@ -6,7 +6,12 @@ const Select = props => {
   return (
     <div className={styles.control}>
       <label htmlFor={props.id}>{props.label}</label>
-      <select onChange={props.onChanged} name="spotTypes" id={props.id}>
+      <select
+        value={props.value}
+        onChange={props.onChanged}
+        name="spotTypes"
+        id={props.id}
+      >
         <option value="picnic">Picnic</option>
         <option value="camping">Camping</option>
       </select>
