cd66ba3936aec45e93f3947a314f56c0ca59dab2
dikxak <dikshakpoudel76@gmail.com>
2022-08-17 23:24:24 +0545

    add loading spinner to all blogs page

--- src/pages/AllBlogPage/AllBlogPage.js
+++ src/pages/AllBlogPage/AllBlogPage.js
@@ -1,41 +1,48 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 
-import axios from "axios";
+import axios from 'axios';
 
-import Footer from "../../components/ui/Footer/Footer";
-import Navbar from "../../components/ui/Navbar/Navbar";
-import BlogSection from "../HomePage/BlogSection/BlogSection";
+import Footer from '../../components/ui/Footer/Footer';
+import Navbar from '../../components/ui/Navbar/Navbar';
+import BlogSection from '../HomePage/BlogSection/BlogSection';
+import LoadingContext from '../../context/LoadingSpinnerContext/loading-context';
+import LoadingSpinner from '../../components/ui/LoadingSpinner/LoadingSpinner';
 
-import styles from "./AllBlogPage.module.css";
+import styles from './AllBlogPage.module.css';
 
 const AllBlogPage = () => {
+  const { isLoading, setIsLoading } = useContext(LoadingContext);
   const [blogData, setBlogData] = useState([]);
 
   const getBlogData = useCallback(async () => {
     const res = await axios.get(
-      "https://paradisecamp-backend.herokuapp.com/blogs/all"
+      'https://paradisecamp-backend.herokuapp.com/blogs/all'
     );
     return res.data.data;
   }, []);
 
   useEffect(() => {
     const setData = async () => {
+      setIsLoading(true);
       const data = await getBlogData();
       setBlogData(data);
+      setIsLoading(false);
     };
 
     setData();
-  }, [getBlogData]);
+  }, [setIsLoading, getBlogData]);
 
-  return (
+  return isLoading ? (
+    <LoadingSpinner />
+  ) : (
     <React.Fragment>
       <Navbar />
       {blogData.length === 0 ? (
-        ""
+        ''
       ) : (
         <BlogSection
-          headingStyle={styles["heading-style"]}
-          heading={"All Blogs"}
+          headingStyle={styles['heading-style']}
+          heading={'All Blogs'}
           blogData={blogData}
         />
       )}
